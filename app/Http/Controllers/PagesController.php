4f0d04a475050e7d14d77b8d320e802f87a7bbbc
Artenes Nogueira <artenesama@gmail.com>
2017-08-10 12:52:09 -0300

    Creates default layout template

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Larabook\Http\Controllers;
+
+class PagesController extends Controller
+{
+
+    public function home()
+    {
+
+        return view('pages.home');
+
+    }
+
+}
