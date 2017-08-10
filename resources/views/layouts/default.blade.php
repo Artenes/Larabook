f4a596299d03f6a5e3136f97626d888ff3d95c0c
Artenes Nogueira <artenesama@gmail.com>
2017-08-10 13:22:02 -0300

    Setup front-end
    - setup webpack
    - improve home view

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -6,10 +6,11 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
-          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 </head>
 <body>
+
+    @include('layouts.partials.nav')
 
     <div class="container">
 
@@ -17,8 +18,7 @@
 
     </div>
 
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
-        integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa"
-        crossorigin="anonymous"></script>
+    <script src="{{ asset('js/app.js') }}"></script>
+
 </body>
 </html>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,12 @@
 
 @section('content')
 
-    Welcome to Larabook!
+    <div class="jumbotron">
+        <h1>{{ config('app.name') }}</h1>
+        <p>Welcome to the premier place to talk about Laravel with others. Why don't you sign up to see what the fuss is about?</p>
+        <p>
+            <a class="btn btn-lg btn-primary" href="#" role="button">Sign up »</a>
+        </p>
+    </div>
 
 @endsection
